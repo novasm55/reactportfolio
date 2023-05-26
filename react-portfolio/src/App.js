244643cae1fc18bc9075e47ee287a3f785edba62
novasm55 <novasmichael93@gmail.com>
2023-05-26 13:13:41 -0700

    skeleton+ file

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <Switch>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+                <Route exact path="/portfolio">
+                    <Portfolio />
+                </Route>
+                <Route exact path="/contact">
+                    <Contact />
+                </Route>
+                <Route exact path="/resume">
+                    <Resume />
+                </Route>
+                <Route exact path="/">
+                    <About />
+                </Route>
+            </Switch>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
