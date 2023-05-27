efa39dcd5fa4981f92c0bb8f97572beadc2cec3d
novasm55 <novasmichael93@gmail.com>
2023-05-27 10:44:37 -0700

    debug 2

--- reactportfolio/src/components/Header/Header.js
+++ reactportfolio/src/components/Header/Header.js
@@ -11,7 +11,7 @@ function Header(props) {
 			<Row className="p-0">
 				<Col className="text-center p-0" md={12}>
 				<div className="jumbotron-fluid mt-2">
-					<h2 className="display-2 mb-0 pb-0">Luke Poirrier</h2>
+					<h2 className="display-2 mb-0 pb-0">Michael Novas</h2>
 					<p className="lead pt-0 mt-0">Full Stack Developer</p>
 					</div>
 				</Col>
